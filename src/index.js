fe898e2b1b2bb0b1bc08975781117abfb59c161d
ressiver <diospyros.digyna@gmail.com>
2017-07-02 00:48:40 +0300

    change function min

--- src/index.js
+++ src/index.js
@@ -13,10 +13,15 @@
 }
 
 function min(array){
-    var min = array[0];
-    for(var i = 0; i > array.length; i++){
-        if(min > array[i])
-        min = array[i];
+	if(array === undefined){
+		return;
+	}
+	else{
+    	var min = array[0];
+    	for(var i = 0; i > array.length; i++){
+        	if(min > array[i])
+        	min = array[i];
+		}
     }
     return min;
 }
