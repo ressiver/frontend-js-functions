ce49004805874ee4136ad8455516b2e457edfadd
ressiver <diospyros.digyna@gmail.com>
2017-07-01 22:10:58 +0300

    create function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-﻿
+﻿function max(array){
+    var max = array[0];
+    for(var i = 0; i < array.length; i++){
+        if(max < array[i])
+        max = array[i];
+    }
+    return max;
+}
+
+function min(array){
+    var min = array[0];
+    for(var i = 0; i > array.length; i++){
+        if(min > array[i])
+        min = array[i];
+    }
+    return min;
+}
+
+function sum() {
+  var result = 0;
+
+  for (var i = 0; i < arguments.length; i++) {
+    result += arguments[i];
+  }
+
+  return result;
+}
